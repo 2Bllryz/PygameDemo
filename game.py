9d5989d84eb1a95140a776370db36348728bc6db
24510 <2451073159@qq.com>
2025-02-08 07:28:47 -0800

    第二次提交

---
+++ game.py
@@ -0,0 +1,24 @@
+#Imports
+import pygame, sys
+from pygame.locals import *
+import random, time
+
+#Initialzing
+pygame.init()
+
+#Setting up FPS
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+#Creating colors
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+#Other Variables for use in the program
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+SCORE = 0
